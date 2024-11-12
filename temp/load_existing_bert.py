ea46bb8ca5f782f4ca07b1f3050f2cbd544119f0
nscc_yang0886 <yang0886@e.ntu.edu.sg>
2024-11-12 21:45:10 +0800

    inference success

--- temp/load_existing_bert.py
+++ temp/load_existing_bert.py
@@ -8,10 +8,16 @@ from sklearn.model_selection import train_test_split
 import optuna
 import torch
 import pandas
+from torch.utils.data import DataLoader
+from sklearn.metrics import accuracy_score
 
-finetuned = os.path.join('checkpoint','bert','final_model')
-labeled_data = os.path.join('data','train.json')
-unlabeled_data = os.path.join('data','test.json')
+
+naame = 'roberta'
+
+prefix = '/home/users/ntu/yang0886/proj/Bi-Senti-EE6483/'
+finetuned = os.path.join(prefix,'checkpoint',naame,'final_model')
+labeled_data = os.path.join(prefix,'data','train.json')
+unlabeled_data = os.path.join(prefix,'data','test.json')
 
 if os.path.exists(finetuned):
     
@@ -19,8 +25,8 @@ if os.path.exists(finetuned):
     df1 = pandas.read_json(labeled_data)
     df2 = pandas.read_json(unlabeled_data)
 
-    df1_500 = df1[:500]
-    df2_33 = df2[:33]
+    df1_500 = df1
+    df2_33 = df2
 
     # to dataset
     val_dataset = Dataset.from_dict({
@@ -40,13 +46,33 @@ if os.path.exists(finetuned):
     # token
     tokenized_val_dataset = val_dataset.map(lambda x: tokenize_function(x, tokenizer), batched=True)
     tokenized_pred_dataset = pred_dataset.map(lambda x: tokenize_function(x, tokenizer), batched=True)
-    
+    tokenized_val_dataset = tokenized_val_dataset.remove_columns(["text","label"])
+    tokenized_pred_dataset = tokenized_pred_dataset.remove_columns(["text"])
+
     device = "cuda" if torch.cuda.is_available() else "cpu"
     model = model.to(device)
     model.eval()  # 设置为评估模式
 
     with torch.no_grad():
-        outputs = model(**{k: v.to(device) for k, v in tokenized_val_dataset})
-        predictions = torch.nn.functional.softmax(outputs.logits, dim=-1)
-        predicted_class = torch.argmax(predictions, dim=-1).cpu().numpy()
-        confidence = predictions[range(len(predicted_class)), predicted_class].cpu().numpy()
+
+        output_val = []
+        for i in range(len(tokenized_val_dataset)):
+            current_input = {k: torch.tensor(v).unsqueeze(0).to(device) for k, v in tokenized_val_dataset[i].items()}
+            outputs = model(**current_input)
+            prediction_logits = torch.nn.functional.softmax(outputs.logits, dim=-1)
+            prediction_class = torch.argmax(prediction_logits, dim=-1).item()
+            output_val.append(prediction_class)
+        
+        print('val acc:',accuracy_score(df1_500["sentiments"].tolist(),output_val))
+
+        output_pred = []
+        for i in range(len(tokenized_pred_dataset)):
+            current_input = {k: torch.tensor(v).unsqueeze(0).to(device) for k, v in tokenized_pred_dataset[i].items()}
+            outputs = model(**current_input)
+            prediction_logits = torch.nn.functional.softmax(outputs.logits, dim=-1)
+            prediction_class = torch.argmax(prediction_logits, dim=-1).item()
+            output_pred.append(prediction_class)
+        
+        s = pandas.Series(output_pred)
+        df2['sentiments'] = s
+        df2.to_csv(f'{naame}.csv',index=False)
